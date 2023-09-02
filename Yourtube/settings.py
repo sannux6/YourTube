681046db0c914da563c8a2747f61c8fa9b8509ec
sanjay <paharisanjay10@gmail.com>
2023-09-02 22:18:40 +0545

    base

--- Yourtube/settings.py
+++ Yourtube/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "base.apps.BaseConfig",
+     "tailwind",
+     'compressor',
     
 
 ]
@@ -58,7 +60,7 @@ ROOT_URLCONF = "Yourtube.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -125,3 +127,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+COMPRESS_ROOT = BASE_DIR / 'static'
+  
+COMPRESS_ENABLED = True
+  
+STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
